2987fcdfdac4f4907072337b3d37538d5b7d6dfd
DDomjosa <DDomjosa@gmail.com>
2015-11-28 15:13:45 +0200

    Update One time pad encryption.cpp

--- One time pad encryption.cpp
+++ One time pad encryption.cpp
@@ -6,12 +6,22 @@
 #include <time.h>
 #include <vector>
 #include <windows.h>
-bool inputFromFile(std::string fileName, std::vector<unsigned char>& chars)
+std::string base64_decode(std::string const&);
+static const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+bool inputFromFile(std::string fileName, std::vector<unsigned char>& chars, bool plainText)
 {
     char input;
     std::ifstream read(fileName);
     while (read.get(input))
         chars.push_back(input);
+    if (!plainText)
+    {
+        std::string base64String(chars.begin(), chars.end());
+        chars.clear();
+        std::string rawString = base64_decode(base64String);
+        base64String.clear();
+        std::copy(rawString.begin(), rawString.end(), std::back_inserter(chars));
+    }
     if (!chars.size())
     {
         std::cout << std::endl << "'" << fileName << "' doesn't exist or is empty!";
@@ -32,6 +42,80 @@ int getInt(int minimum, int maximum)
         else
             std::cout << std::endl << "Error! Try again: ";
 	}
+}
+static inline bool is_base64(unsigned char c)
+{
+    return (isalnum(c) || (c == '+') || (c == '/'));
+}
+std::string base64_decode(std::string const& encoded_string)
+{
+    size_t in_len = encoded_string.size(), i = 0, j = 0;
+    int in_ = 0;
+    unsigned char char_array_4[4], char_array_3[3];
+    std::string ret;
+    while (in_len-- && (encoded_string[in_] != '=') && is_base64(encoded_string[in_]))
+    {
+        char_array_4[i++] = encoded_string[in_];
+        in_++;
+        if (i == 4)
+        {
+            for (i=0;i<4;i++)
+                char_array_4[i] = static_cast<unsigned char>(base64_chars.find(char_array_4[i]));
+            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+            for (i=0;i<3;i++)
+                ret += char_array_3[i];
+            i = 0;
+        }
+    }
+    if (i)
+    {
+        for (j=i;j<4;j++)
+            char_array_4[j] = 0;
+        for (j=0;j<4;j++)
+            char_array_4[j] = static_cast<unsigned char>(base64_chars.find(char_array_4[j]));
+        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+        char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+        for (j=0;j<i-1;j++)
+            ret += char_array_3[j];
+    }
+    return ret;
+}
+std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len)
+{
+    std::string ret;
+    int i = 0, j = 0;
+    unsigned char char_array_3[3], char_array_4[4];
+    while (in_len--)
+    {
+        char_array_3[i++] = *(bytes_to_encode++);
+        if (i == 3)
+        {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
+            for (i=0;i<4;i++)
+                ret += base64_chars[char_array_4[i]];
+            i = 0;
+        }
+    }
+    if (i)
+    {
+        for(j=i;j<3;j++)
+            char_array_3[j] = '\0';
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[3] = char_array_3[2] & 0x3f;
+        for (j=0;j<i+1;j++)
+            ret += base64_chars[char_array_4[j]];
+        while (i++<3)
+            ret += '=';
+    }
+    return ret;
 }
 unsigned char keyChar(std::mt19937& randomGenerator, std::uniform_int_distribution<int>& randomNumber)
 {
@@ -97,7 +181,7 @@ int main()
             {
             case 1:
                 {
-                    if (inputFromFile("Input\\(E)Plain text.txt", plainTextChars))
+                    if (inputFromFile("Input\\(E)Plain text.txt", plainTextChars, true))
                     {
                         for (int i=0;i<plainTextChars.size();i++)
                             keyChars.push_back(keyChar(randomGenerator, randomNumber));
@@ -109,7 +193,7 @@ int main()
                 }
             case 2:
                 {
-                    if ((inputFromFile("Input\\(E)Plain text.txt", plainTextChars)) && (inputFromFile("Input\\(E)Key.ddkey", keyChars)))
+                    if ((inputFromFile("Input\\(E)Plain text.txt", plainTextChars, true)) && (inputFromFile("Input\\(E)Key.ddkey", keyChars, false)))
                         xorVectors(plainTextChars, keyChars, ciphertextChars);
                     else
                     {
@@ -123,10 +207,16 @@ int main()
             {
                 std::cout << std::endl << "Key:\t\t'Output\\(E)Key.ddkey'" << std::endl << "Ciphertext:\t'Output\\(E)Ciphertext.ddkey'";
                 std::ofstream keyPrint("Output\\(E)Key.ddkey"), ciphertextPrint("Output\\(E)Ciphertext.ddkey");
-                for (int i=0;i<keyChars.size();i++)
-                    keyPrint << keyChars[i];
-                for (int i=0;i<ciphertextChars.size();i++)
-                    ciphertextPrint << ciphertextChars[i];
+                std::string rawKey(keyChars.begin(), keyChars.end());
+                keyChars.clear();
+                std::string base64Key = base64_encode(reinterpret_cast<const unsigned char*>(rawKey.c_str()), rawKey.length());
+                rawKey.clear();
+                keyPrint << base64Key;
+                std::string rawCiphertext(ciphertextChars.begin(), ciphertextChars.end());
+                ciphertextChars.clear();
+                std::string base64Ciphertext = base64_encode(reinterpret_cast<const unsigned char*>(rawCiphertext.c_str()), rawCiphertext.length());
+                rawCiphertext.clear();
+                ciphertextPrint << base64Ciphertext;
             }
             break;
         }
@@ -135,7 +225,7 @@ int main()
             std::cout << std::endl << "Key: 'Input\\(D)Key.ddkey';" << std::endl << "Ciphertext: 'Input\\(D)Ciphertext.ddkey'.";
             std::cout << std::endl << "Press ENTER after the files are ready! ";
             ignore();
-            if ((inputFromFile("Input\\(D)Key.ddkey", keyChars)) && (inputFromFile("Input\\(D)Ciphertext.ddkey", ciphertextChars)))
+            if ((inputFromFile("Input\\(D)Key.ddkey", keyChars, false)) && (inputFromFile("Input\\(D)Ciphertext.ddkey", ciphertextChars, false)))
                 xorVectors(keyChars, ciphertextChars, plainTextChars);
             else
             {
